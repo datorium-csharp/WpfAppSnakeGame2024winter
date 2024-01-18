612c9e80b5ec5015bf9f5fa7675326009abfdbb6
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2024-01-18 18:54:17 +0200

    Add project files.

---
+++ WpfAppSnakeGame2024winter/SnakePart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppSnakeGame2024winter
+{
+    internal class SnakePart
+    {
+
+
+
+
+    }
+}
