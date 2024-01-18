b09b9539accb8b4ad61d322b745c97c61f4c7951
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2024-01-18 19:13:11 +0200

    Movement functionality ir ready

--- WpfAppSnakeGame2024winter/SnakePart.cs
+++ WpfAppSnakeGame2024winter/SnakePart.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace WpfAppSnakeGame2024winter
 {
     internal class SnakePart
     {
-
-
-
+        public UIElement UiElement { get; set; }
+        public Point Position { get; set; }
+        public bool IsHead { get; set; }
 
     }
 }
